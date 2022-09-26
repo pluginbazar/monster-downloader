0feef5279fb1dcd8c3dd3d1d59bcaa51eef82ae5
Jaed <jaedm97@gmail.com>
2022-09-26 15:14:04 +0600

    functionality ready

---
+++ assets/admin/js/scripts.js
@@ -0,0 +1,70 @@
+/**
+ * Front Script
+ */
+
+(function ($, window, document, pluginObject) {
+    "use strict";
+
+    let getUrlParameter = function getUrlParameter(sParam) {
+            let sPageURL = window.location.search.substring(1),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
+
+            for (i = 0; i < sURLVariables.length; i++) {
+                sParameterName = sURLVariables[i].split('=');
+
+                if (sParameterName[0] === sParam) {
+                    return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+                }
+            }
+            return false;
+        },
+        getCurrentPage = function getCurrentpage() {
+            return window.location.pathname.split('/').at(-1);
+        };
+
+
+    $(document).on('focus', '.theme-overlay', function (e) {
+
+        if ('themes.php' !== getCurrentPage()) {
+            return;
+        }
+
+        let themeActions = $(e.target).find('.theme-actions'),
+            themeActionsActive = themeActions.find('.active-theme'),
+            themeActionsInActive = themeActions.find('.inactive-theme'),
+            activeTheme = getUrlParameter('theme'),
+            themeDownloadLink = pluginObject.themeDownloadLink.replace('object_name', activeTheme),
+            themeDownloadBtn = '<a class="button" href="' + themeDownloadLink + '">' + pluginObject.themeDownloadText + '</a>';
+
+        if (activeTheme.length > 0) {
+            themeActionsActive.append(themeDownloadBtn);
+            themeActionsInActive.append(themeDownloadBtn);
+        }
+    });
+
+
+    $(document).on('ready', function () {
+
+        if ('themes.php' !== getCurrentPage()) {
+            return;
+        }
+
+        setTimeout(function () {
+
+            let themesAll = $('.themes'),
+                themeDownloadBtnSample = '<a class="button" href="' + pluginObject.themeDownloadLink + '">' + pluginObject.themeDownloadText + '</a>';
+
+            themesAll.find('> .theme').each(function () {
+                let themeSlug = $(this).data('slug');
+
+                if (typeof themeSlug !== 'undefined') {
+                    $(this).find('.theme-id-container .theme-actions').append(themeDownloadBtnSample.replace('object_name', themeSlug));
+                }
+            });
+        }, 50);
+    });
+
+
+})(jQuery, window, document, wpdp);
--- wp-downloader-plus.php
+++ wp-downloader-plus.php
@@ -1,26 +1,163 @@
 <?php
-/**
- * Plugin Name: WP Downloader Plus
- * Plugin URI: http://pluginbazar.com/
- * Description: This plugin for download WordPress plugin and theme.
- * Version: 1.0.0
- * Author: Pluginbazar
- * Author URI: http://pluginbazar.com/
- *
- */
+/*
+	Plugin Name: WP Downloader Plus
+	Plugin URI: https://pluginbazar.com/
+	Description: This plugin for download WordPress plugin and theme.
+	Version: 1.0.0
+	Author: Pluginbazar
+	Text Domain: woc-order-alert
+	Author URI: https://pluginbazar.com/
+	License: GPLv2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
 
 defined( 'ABSPATH' ) || exit;
+defined( 'WPDB_PLUGIN_URL' ) || define( 'WPDB_PLUGIN_URL', WP_PLUGIN_URL . '/' . plugin_basename( dirname( __FILE__ ) ) . '/' );
+defined( 'WPDB_PLUGIN_DIR' ) || define( 'WPDB_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+defined( 'WPDB_PLUGIN_FILE' ) || define( 'WPDB_PLUGIN_FILE', plugin_basename( __FILE__ ) );
+defined( 'WPDB_PLUGIN_VERSION' ) || define( 'WPDB_PLUGIN_VERSION', '1.0.0' );
+
 if ( ! class_exists( 'WPDP_Main' ) ) {
 	/**
 	 * Class WPDP_Main
 	 */
 	class WPDP_Main {
+
 		protected static $_instance = null;
 
+		protected static $_script_version = null;
+
+		/**
+		 * WPDP_Main constructor.
+		 */
 		function __construct() {
-			add_action( 'plugins_loaded', array( $this, 'wpdb_loaded_hooks' ), 10, 3 );
+
+			self::$_script_version = defined( 'WP_DEBUG' ) && WP_DEBUG ? current_time( 'U' ) : WPDB_PLUGIN_VERSION;
+
+			add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
+			add_filter( 'plugin_action_links', array( $this, 'add_download_btn_to_plugins' ), 10, 4 );
+			add_action( 'admin_init', array( $this, 'download_object' ) );
 		}
 
+
+		/**
+		 * Handle downloading the object
+		 */
+		function download_object() {
+
+			if ( ! isset( $_GET['wpdp'] ) || ! wp_verify_nonce( $_GET['_wpnonce'], 'wpdp-download' ) ) {
+				return;
+			}
+
+			if ( ! class_exists( 'PclZip' ) ) {
+				include ABSPATH . 'wp-admin/includes/class-pclzip.php';
+			}
+
+			$context = isset( $_GET['wpdp'] ) ? sanitize_text_field( $_GET['wpdp'] ) : '';
+			$object  = isset( $_GET['object'] ) ? sanitize_text_field( $_GET['object'] ) : '';
+
+			switch ( $context ) {
+				case 'plugin':
+					if ( strpos( $object, '/' ) ) {
+						$object = dirname( $object );
+					}
+					$root = WP_PLUGIN_DIR;
+					break;
+				case 'muplugin':
+					if ( strpos( $object, '/' ) ) {
+						$object = dirname( $object );
+					}
+					$root = WPMU_PLUGIN_DIR;
+					break;
+				case 'theme':
+					$root = get_theme_root( $object );
+					break;
+				default:
+					wp_die( esc_html__( 'Something went wrong!', 'wp-downloader-plus' ) );
+			}
+
+			$object = sanitize_file_name( $object );
+
+			if ( empty( $object ) ) {
+				wp_die( esc_html__( 'Something went wrong!', 'wp-downloader-plus' ) );
+			}
+
+			$path       = $root . '/' . $object;
+			$fileName   = $object . '.zip';
+			$upload_dir = wp_upload_dir();
+			$tmpFile    = trailingslashit( $upload_dir['path'] ) . $fileName;
+			$archive    = new PclZip( $tmpFile );
+
+			$archive->add( $path, PCLZIP_OPT_REMOVE_PATH, $root );
+
+			header( 'Content-type: application/zip' );
+			header( 'Content-Disposition: attachment; filename="' . $fileName . '"' );
+
+			readfile( $tmpFile );
+			unlink( $tmpFile );
+
+			exit;
+		}
+
+
+		/**
+		 * Add download button to plugins list page.
+		 *
+		 * @param $links
+		 * @param $file
+		 * @param $plugin_data
+		 * @param $context
+		 *
+		 * @return mixed
+		 */
+		function add_download_btn_to_plugins( $links, $file, $plugin_data, $context ) {
+
+			if ( 'dropins' === $context ) {
+				return $links;
+			}
+
+			$what = ( 'mustuse' === $context ) ? 'muplugin' : 'plugin';
+
+			$links['wpdp-download'] = sprintf( '<a href="%s">%s</a>', $this->get_object_download_link( $file, $what ), esc_html__( 'Download' ) );
+
+			return $links;
+		}
+
+
+		/**
+		 * Return object download link
+		 *
+		 * @param string $object
+		 * @param string $object_type
+		 *
+		 * @return mixed|void
+		 */
+		function get_object_download_link( $download_object = '', $object_type = 'plugin' ) {
+
+			$download_object = empty( $download_object ) ? 'object_name' : $download_object;
+			$download_query  = build_query( array( 'wpdp' => $object_type, 'object' => $download_object ) );
+			$download_link   = wp_nonce_url( admin_url( '?' . $download_query ), 'wpdp-download' );
+
+			return apply_filters( 'WPDP_Main/Filters/get_object_download_link', $download_link, $download_object, $object_type );
+		}
+
+		/**
+		 * Admin Scripts
+		 */
+		function admin_scripts() {
+
+			wp_enqueue_script( 'wpdp-admin', plugins_url( '/assets/admin/js/scripts.js', __FILE__ ), array( 'jquery' ), self::$_script_version, true );
+			wp_localize_script( 'wpdp-admin', 'wpdp', array(
+				'ajaxUrl'           => admin_url( 'admin-ajax.php' ),
+				'themeDownloadText' => esc_html__( 'Download' ),
+				'themeDownloadLink' => $this->get_object_download_link( '', 'theme' ),
+			) );
+		}
+
+
+		/**
+		 * @return WPDP_Main|null
+		 */
 		public static function instance() {
 			if ( is_null( self::$_instance ) ) {
 				self::$_instance = new self();
@@ -28,14 +165,7 @@ if ( ! class_exists( 'WPDP_Main' ) ) {
 
 			return self::$_instance;
 		}
-
-		/**
-		 * @return void
-		 */
-		function wpdb_loaded_hooks() {
-			add_filter( 'plugin_action_links', array( $this, 'wpdp_plugin_action' ) );
-		}
-
-	
 	}
 }
+
+WPDP_Main::instance();
